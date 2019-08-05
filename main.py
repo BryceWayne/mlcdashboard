787490cd9c6ff16f6c4d29780d95c2c3be06c6f2
Batman <35088752+BryceWayne@users.noreply.github.com>
2019-08-05 10:45:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,58 @@
-import logging
+'''This example demonstrates embedding an autoloaded Bokeh server
+into a simple Flask application, and passing arguments to Bokeh.
+To view the example, run:
+    python flask_server.py
+in this directory, and navigate to:
+    http://localhost:5000
+'''
+import atexit
+import subprocess
 
-from flask import Flask
+from flask import render_template_string, Flask
 from bokeh.embed import server_document
-script = server_document("https://demo.bokeh.org/sliders")
+
+home_html = """
+<!DOCTYPE html>
+<html lang="en">
+  <body>
+    <div class="bk-root">
+      <h1><a href="/batch/1"> Batch 1 (cos)</a></h1>
+      <h1><a href="/batch/2"> Batch 2 (sin)</a></h1>
+      <h1><a href="/batch/3"> Batch 3 (tan)</a></h1>
+    </div>
+  </body>
+</html>
+"""
+
+app_html = """
+<!DOCTYPE html>
+<html lang="en">
+  <body>
+    <div>
+        <h2><a href="/batch/1">Batch 1 (cos)</a> - <a href="/batch/2">Batch 2 (sin)</a> - <a href="/batch/3">Batch 3 (tan)</a></h2>
+    </div>
+    {{ bokeh_script|safe }}
+  </body>
+</html>
+"""
 
 app = Flask(__name__)
 
+bokeh_process = subprocess.Popen(
+    ['python', '-m', 'bokeh', 'serve', '--allow-websocket-origin=localhost:5000', 'bokeh_server.py'], stdout=subprocess.PIPE)
+
+@atexit.register
+def kill_server():
+    bokeh_process.kill()
 
 @app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+def home():
+    return render_template_string(home_html)
 
-
-@app.errorhandler(500)
-def server_error(e):
-    logging.exception('An error occurred during a request.')
-    return """
-    An internal error occurred: <pre>{}</pre>
-    See logs for full stacktrace.
-    """.format(e), 500
-
+@app.route('/batch/<int:batchid>')
+def visualization(batchid):
+    bokeh_script = server_document(url='http://localhost:5006/bokeh_server', arguments=dict(batchid=batchid))
+    return render_template_string(app_html, bokeh_script=bokeh_script)
 
 if __name__ == '__main__':
-    # This is used when running locally. Gunicorn is used to run the
-    # application on Google App Engine. See entrypoint in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
