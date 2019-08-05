9776f8e7bcf712a428e9ca52ec355091707a0702
Batman <35088752+BryceWayne@users.noreply.github.com>
2019-08-05 09:31:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,69 @@
-from numpy.random import random
-from bokeh.io import curdoc
+import flask
+
+from bokeh.embed import components
 from bokeh.plotting import figure
-from bokeh.layouts import column, widgetbox
-from bokeh.models import Button, ColumnDataSource
-from bokeh.server.server import Server
+from bokeh.resources import INLINE
+from bokeh.templates import RESOURCES
+from bokeh.util.string import encode_utf8
 
-"""
-create and run a demo bokeh app on a cloud server
-"""
+app = flask.Flask(__name__)
 
-def run(doc):
+colors = {
+    'Black': '#000000',
+    'Red':   '#FF0000',
+    'Green': '#00FF00',
+    'Blue':  '#0000FF',
+}
 
-    fig = figure(title='random data', width=400, height=200, tools='pan,box_zoom,reset,save')
 
-    source = ColumnDataSource(data={'x': [], 'y': []})
-    fig.line('x', 'y', source=source)
+def getitem(obj, item, default):
+    if item not in obj:
+        return default
+    else:
+        return obj[item]
 
-    def click(n=100):
-        source.data = {'x': range(n), 'y': random(n)}
 
-    button = Button(label='update', button_type='success')
-    button.on_click(click)
+@app.route("/")
+def polynomial():
+    """ Very simple embedding of a polynomial chart"""
+    # Grab the inputs arguments from the URL
+    # This is automated by the button
+    args = flask.request.args
 
-    layout = column(widgetbox(button), fig)
-    doc.add_root(layout)
-    click()
+    # Get all the form arguments in the url with defaults
+    color = colors[getitem(args, 'color', 'Black')]
+    _from = int(getitem(args, '_from', 0))
+    to = int(getitem(args, 'to', 10))
 
-# configure and run bokeh server
-kws = {'port': 5100, 'prefix': '/bokeh', 'allow_websocket_origin': ['165.227.26.215']}
-server = Server(run, **kws)
-server.start()
-if __name__ == '__main__':
-    server.io_loop.add_callback(server.show, '/')
-    server.io_loop.start()
+    # Create a polynomial line graph
+    x = list(range(_from, to + 1))
+    fig = figure(title="Polynomial")
+    fig.line(x, [i ** 2 for i in x], color=color, line_width=2)
+
+    # Configure resources to include BokehJS inline in the document.
+    # For more details see:
+    #   http://bokeh.pydata.org/en/latest/docs/reference/resources_embedding.html#module-bokeh.resources
+    plot_resources = RESOURCES.render(
+        js_raw=INLINE.js_raw,
+        css_raw=INLINE.css_raw,
+        js_files=INLINE.js_files,
+        css_files=INLINE.css_files,
+    )
+
+    # For more details see:
+    #   http://bokeh.pydata.org/en/latest/docs/user_guide/embedding.html#components
+    script, div = components(fig, INLINE)
+    html = flask.render_template(
+        'embed.html',
+        plot_script=script, plot_div=div, plot_resources=plot_resources,
+        color=color, _from=_from, to=to
+    )
+    return encode_utf8(html)
+
+
+def main():
+    app.debug = True
+    app.run()
+
+if __name__ == "__main__":
+    main()
