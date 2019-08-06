c41752845e6ecb21ebf42a8f3ee329f87f1be307
Batman <35088752+BryceWayne@users.noreply.github.com>
2019-08-06 10:23:35 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    script=autoload_server(model=None,app_path="/bokeh-sliders",url="http://localhost:5006")
+    #script=autoload_server(model=None,app_path="/bokeh-sliders",url="http://localhost:5006")
+    script=server_document("http://localhost:5006/bokeh-sliders")
+    print(script)
     return render_template('hello.html',bokS=script)
 
 if __name__ == "__main__":
