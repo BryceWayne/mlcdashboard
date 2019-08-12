572e23931dc8e7bc5560a6ecdae2da08ab36eb21
Batman <35088752+BryceWayne@users.noreply.github.com>
2019-08-12 11:00:38 -0700

    Attempt 2

--- main.py
+++ main.py
@@ -35,48 +35,6 @@ def index():
 	# p.legend.location = "top_center"
 	"""
 
-	"""Attempt 2"""
-	"""
-	x = np.linspace(0, 10, 500)
-	y = np.sin(x)
-
-	source = ColumnDataSource(data=dict(x=x, y=y))
-
-	p = figure(y_range=(-10, 10), plot_width=1000, plot_height=600)
-
-	p.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
-
-	amp_slider = Slider(start=0.1, end=10, value=1, step=.1, title="Amplitude")
-	freq_slider = Slider(start=0.1, end=10, value=1, step=.1, title="Frequency")
-	phase_slider = Slider(start=0, end=6.4, value=0, step=.1, title="Phase")
-	offset_slider = Slider(start=-5, end=5, value=0, step=.1, title="Offset")
-
-	#callback = CustomJS(args=dict(source=source, amp=amp_slider, freq=freq_slider, phase=phase_slider, offset=offset_slider),
-	#	                code="""
-	#							const data = source.data;
-	#							const A = amp.value;
-	#							const k = freq.value;
-	#							const phi = phase.value;
-	#							const B = offset.value;
-	#							const x = data['x']
-	#							const y = data['y']
-	#							for (var i = 0; i < x.length; i++) {
-	#							    y[i] = B + A*Math.sin(k*x[i]+phi);
-	#							}
-	#							source.change.emit();
-								""")
-
-	amp_slider.js_on_change('value', callback)
-	freq_slider.js_on_change('value', callback)
-	phase_slider.js_on_change('value', callback)
-	offset_slider.js_on_change('value', callback)
-
-	layout = row(
-	    p,
-	    column(amp_slider, freq_slider, phase_slider, offset_slider),
-	)
-	"""
-
 	"""
 	Attempt 3
 	"""
