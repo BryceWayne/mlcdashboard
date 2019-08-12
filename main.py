2d9f96066977d846b4534c4fafa490cf936eb50e
Batman <35088752+BryceWayne@users.noreply.github.com>
2019-08-12 10:52:52 -0700

    Added Imports

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from bokeh.embed import components
 from bokeh.plotting import figure, output_file, show, ColumnDataSource
 from bokeh.resources import INLINE
 from bokeh.util.string import encode_utf8
-from bokeh.io import show, output_file
-from bokeh.models import ColumnDataSource, CustomJS, Slider
+from bokeh.io import show, output_file, curdoc
+from bokeh.models import ColumnDataSource, CustomJS, Slider, Range1d
+from bokeh.models.widgets import Slider, TextInput, Tabs, Panel, Button, DataTable
+from bokeh.models.widgets import NumberFormatter, TableColumn, Dropdown, RadioButtonGroup, Select
 from bokeh.palettes import Spectral6
 from bokeh.transform import factor_cmap
 from bokeh.layouts import row, column
