b3415acde86304139534895b94a2f552cee444fd
Batman <35088752+BryceWayne@users.noreply.github.com>
2019-08-13 09:25:12 -0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,16 @@
+from bokeh.embed import components
+from bokeh.plotting import figure, output_file, show, ColumnDataSource
+from bokeh.resources import INLINE
+from bokeh.util.string import encode_utf8
+from bokeh.io import show, output_file, curdoc
+from bokeh.embed import server_document
+from bokeh.models import ColumnDataSource, CustomJS, Slider, Range1d
+from bokeh.models.widgets import Slider, TextInput, Tabs, Panel, Button, DataTable
+from bokeh.models.widgets import NumberFormatter, TableColumn, Dropdown, RadioButtonGroup, Select
+from bokeh.palettes import Spectral6
+from bokeh.transform import factor_cmap
+from bokeh.layouts import row, column
+import numpy as np
 # Set up data
 	N = 1000
 	x1 = np.linspace(-10, 10, N)
