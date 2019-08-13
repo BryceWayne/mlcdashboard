336e3855e751c857f1b9c78f387c08abd2d608e3
Batman <35088752+BryceWayne@users.noreply.github.com>
2019-08-13 09:24:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 from flask import Flask, render_template
-from bokeh.embed import components
-from bokeh.plotting import figure, output_file, show, ColumnDataSource
-from bokeh.resources import INLINE
-from bokeh.util.string import encode_utf8
-from bokeh.io import show, output_file, curdoc
 from bokeh.embed import server_document
-from bokeh.models import ColumnDataSource, CustomJS, Slider, Range1d
-from bokeh.models.widgets import Slider, TextInput, Tabs, Panel, Button, DataTable
-from bokeh.models.widgets import NumberFormatter, TableColumn, Dropdown, RadioButtonGroup, Select
-from bokeh.palettes import Spectral6
-from bokeh.transform import factor_cmap
-from bokeh.layouts import row, column
-import numpy as np
 
 app = Flask(__name__)
 
