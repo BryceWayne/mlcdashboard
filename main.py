6ef6ee393cfe1f0bcdf9c0d25a7ca7cc56ffa043
Batman <35088752+BryceWayne@users.noreply.github.com>
2019-08-14 11:14:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from bokeh.embed import server_document
 from bokeh.util.string import encode_utf8
 import subprocess
+import atexit
 
 app = Flask(__name__)
 
@@ -10,11 +11,17 @@ bokeh_process = subprocess.Popen(["bokeh",
 				  "--allow-websocket-origin=*",
 				  "dashboard.py"], stdout=subprocess.PIPE)
 
+@atexit.register
+def kill_server():
+    bokeh_process.kill()
+
 @app.route('/')
 def index():
+	session = pull_session(app_path='/')
+    	bokeh_script = autoload_server(None, app_path="/", session_id=session.id)
 	# render template
-	script = server_document("http://localhost:5006/dashboard")
-	html = render_template('index.html', plot_script=script)
+# 	script = server_document("http://localhost:5006/dashboard")
+	html = render_template('index.html', plot_script=bokeh_script)
 	return encode_utf8(html)
 
 
