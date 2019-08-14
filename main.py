5b736ba17d3dded423f1b68444fffe201ce59f73
Batman <35088752+BryceWayne@users.noreply.github.com>
2019-08-14 16:05:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,25 @@ from flask_sockets import Sockets
 import subprocess
 
 app = Flask(__name__)
+sockets = Sockets(app)
 
 bokeh_process = subprocess.Popen(
     ['bokeh', 'serve','--allow-websocket-origin=*','dashboard.py'], stdout=subprocess.PIPE)
+
+
+@sockets.route('/chat')
+def chat_socket(ws):
+    while not ws.closed:
+        message = ws.receive()
+        if message is None:  # message is "None" if the client has closed.
+            continue
+        # Send the message to all clients connected to this webserver
+        # process. (To support multiple processes or instances, an
+        # extra-instance storage or messaging system would be required.)
+        clients = ws.handler.server.clients.values()
+        for client in clients:
+            client.ws.send(message)
+
 
 @app.route('/')
 def index():
