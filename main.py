27cbb15f651639d5ed7a4083c1c2518e102c33ba
Batman <35088752+BryceWayne@users.noreply.github.com>
2019-08-14 12:41:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
 #main.py
+from bokeh.embed import server_document
+from flask import Flask
 import subprocess
-import atexit
-from flask import render_template, render_template_string, Flask
-from bokeh.embed import autoload_server
-from bokeh.client import pull_session
 
-app_html="""
-<!DOCTYPE html>
-<html lang="en">
-  <body>
-    <div class="bk-root">
-      {{ bokeh_script|safe }}
-    </div>
-  </body>
-</html>
-"""
-
+script = server_document("https://demo.bokeh.org/sliders")
 app = Flask(__name__)
 
 bokeh_process = subprocess.Popen(
@@ -27,10 +15,8 @@ def kill_server():
 
 @app.route('/')
 def index():
-    session=pull_session(app_path='/dashboard')
-    bokeh_script=autoload_server(None,app_path="/dashboard",session_id=session.id)
-    # return render_template('app.html', bokeh_script=bokeh_script)
-    return render_template_string(app_html, bokeh_script=bokeh_script)
+    script = server_document("localhost:5006/dashboard")
+    return render_template_string('index.html', bokeh_script=script)
 
 if __name__ == '__main__':
     app.run(port='8080')
