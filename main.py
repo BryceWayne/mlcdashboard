81b6d7df136261d1907bc7c7ac5ce7467a898b83
Batman <35088752+BryceWayne@users.noreply.github.com>
2019-08-14 11:49:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template
-from bokeh.embed import server_document
-from bokeh.util.string import encode_utf8
+#main.py
 import subprocess
 import atexit
+from flask import render_template, render_template_string, Flask
+from bokeh.embed import autoload_server
+from bokeh.client import pull_session
+
+app_html="""
+<!DOCTYPE html>
+<html lang="en">
+  <body>
+    <div class="bk-root">
+      {{ bokeh_script|safe }}
+    </div>
+  </body>
+</html>
+"""
 
 app = Flask(__name__)
 
-bokeh_process = subprocess.Popen(["bokeh", 
-				  "serve", 
-				  "--allow-websocket-origin=*",
-				  "dashboard.py"], stdout=subprocess.PIPE)
+bokeh_process = subprocess.Popen(
+    ['bokeh', 'serve','--allow-websocket-origin=localhost:5000','bokeh_plot.py'], stdout=subprocess.PIPE)
 
 @atexit.register
 def kill_server():
@@ -17,12 +27,10 @@ def kill_server():
 
 @app.route('/')
 def index():
-	session = pull_session(app_path='/')
-    	bokeh_script = autoload_server(None, app_path="/", session_id=session.id)
-	# render template
-# 	script = server_document("http://localhost:5006/dashboard")
-	html = render_template('index.html', plot_script=bokeh_script)
-	return encode_utf8(html)
+    session=pull_session(app_path='/bokeh_plot')
+    bokeh_script=autoload_server(None,app_path="/bokeh_plot",session_id=session.id)
+    # return render_template('app.html', bokeh_script=bokeh_script)
+    return render_template_string(app_html, bokeh_script=bokeh_script)
 
 if __name__ == '__main__':
     app.run(port='8080')
