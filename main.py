2b0bc82f9051949d8cbcf403dad231e5ed6660d4
Batman <35088752+BryceWayne@users.noreply.github.com>
2019-08-14 10:56:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
 from bokeh.embed import server_document
 from bokeh.util.string import encode_utf8
+import subprocess
+
 app = Flask(__name__)
 
+bokeh_process = subprocess.Popen(["bokeh", 
+				  "serve", 
+				  "--allow-websocket-origin=*",
+				  "dashboard.py"], stdout=subprocess.PIPE)
 
 @app.route('/')
 def index():
