275569ec3e973485b61510c9907cfd3f92c8313a
Batman <35088752+BryceWayne@users.noreply.github.com>
2019-08-19 16:45:46 -0700

    Update

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-web: bokeh serve --port=$PORT --address=0.0.0.0 --allow-websocket-origin=mccmlcdashboard.herokuapp.com --use-xheaders dashboard.py
+web: main.py
+# web: bokeh serve --port=$PORT --address=0.0.0.0:5006 --allow-websocket-origin=mccmlcdashboard.herokuapp.com --use-xheaders dashboard.py
+web: bokeh serve --port=5006 --address=0.0.0.0 --allow-websocket-origin=mccmlcdashboard.herokuapp.com --use-xheaders dashboard.py
--- main.py
+++ main.py
@@ -1,21 +1,10 @@
 #main.py
 from bokeh.embed import server_document
-<<<<<<< HEAD
 from bokeh.util.string import encode_utf8
-=======
 from flask import Flask, request, render_template
->>>>>>> 98fec8ca0a2bc5bfa2ce1c0a1b40298346a1e45b
 import subprocess
 
 app = Flask(__name__)
-
-<<<<<<< HEAD
-subprocess.call("bokeh serve --allow-websocket-origin=* dashboard.py")
-=======
-bokeh_process = subprocess.Popen(
-    ['bokeh', 'serve','--allow-websocket-origin=*', 'dashboard.py'], stdout=subprocess.PIPE)
-
->>>>>>> 98fec8ca0a2bc5bfa2ce1c0a1b40298346a1e45b
 
 @app.route('/')
 def index():
